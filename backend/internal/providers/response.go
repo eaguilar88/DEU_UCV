11b273306236fcdc0f5b1340567247290da65c21
Ellery Aguilar <esaulagj@gmail.com>
2026-03-18 22:39:16 -0600

    fixing file format

--- backend/internal/providers/response.go
+++ backend/internal/providers/response.go
@@ -3,20 +3,20 @@ package providers
 import "github.com/eaguilar88/deu/internal/entities"
 
 type GetProviderResponse struct {
-	ID       string        `json:"proveedor_id"`
-	UserID   string        `json:"usuario_id"`
-	Name     string        `json:"nombre_proveedor"`
-	Bio      string        `json:"biografia,omitempty"`
-	Internal bool          `json:"interno"`
-	Code     string        `json:"codigo_proveedor"`
-	Files    ProviderFiles `json:"archivos"`
-	Logo     string        `json:"provider_avatar_url,omitempty"`
+	ID         string        `json:"proveedor_id"`
+	UserID     string        `json:"usuario_id"`
+	Name       string        `json:"nombre_proveedor"`
+	Bio        string        `json:"biografia,omitempty"`
+	Internal   bool          `json:"interno"`
+	Code       string        `json:"codigo_proveedor"`
+	Files      ProviderFiles `json:"archivos"`
+	Logo       string        `json:"provider_avatar_url,omitempty"`
 	Type       string        `json:"tipo,omitempty"`
 	ProfitType string        `json:"tipo_lucro,omitempty"`
-	Contact  []string      `json:"emails_contacto,omitempty"`
-	Phones   []string      `json:"telefonos_contacto,omitempty"`
-	Webpage  string        `json:"sitio_web,omitempty"`
-	Active   bool          `json:"activo"`
+	Contact    []string      `json:"emails_contacto,omitempty"`
+	Phones     []string      `json:"telefonos_contacto,omitempty"`
+	Webpage    string        `json:"sitio_web,omitempty"`
+	Active     bool          `json:"activo"`
 }
 
 type ProviderFiles struct {
@@ -30,9 +30,9 @@ type ProviderFiles struct {
 // providerToResponse converts a Provider entity to GetProviderResponse.
 func providerToResponse(entity entities.Provider) GetProviderResponse {
 	response := GetProviderResponse{
-		ID:       entity.ID,
-		UserID:   entity.User.ID,
-		Name:     entity.User.FirstName + " " + entity.User.LastName,
+		ID:         entity.ID,
+		UserID:     entity.User.ID,
+		Name:       entity.User.FirstName + " " + entity.User.LastName,
 		Internal:   entity.IsInternal,
 		Code:       entity.Code,
 		Active:     entity.DeletedAt == "",
